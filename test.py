3c634cfa1db43ca6d798787d7e7eb963a68e43ed
Melissa Falcão <falcaomelissa28@gmail.com>
2025-10-06 15:30:25 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.write("i'm back")
-st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Ftonedeaf.thebrag.com%2Fthe-life-of-george-harrison%2F&psig=AOvVaw08KSKf1sGse_pE-44wewL-&ust=1759861597968000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIDxjr2ZkJADFQAAAAAdAAAAABAE", caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
+st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F8303580558855393%2F&psig=AOvVaw0LcsuNiFEXkmXZgBpX4AjK&ust=1759861676282000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPCF-eKZkJADFQAAAAAdAAAAABAE", caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
