7b987e1fe1cf49102f6a65416b04242c103e8228
Melissa Falcão <falcaomelissa28@gmail.com>
2025-10-06 15:32:49 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.write("i'm back")
-st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F8303580558855393%2F&psig=AOvVaw0LcsuNiFEXkmXZgBpX4AjK&ust=1759861676282000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPCF-eKZkJADFQAAAAAdAAAAABAE", caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
+st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F1266706123734727%2F&psig=AOvVaw0EpPXC7p7j-9vbD4C9kKZ9&ust=1759861742217000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMiE-YSakJADFQAAAAAdAAAAABAu", caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
