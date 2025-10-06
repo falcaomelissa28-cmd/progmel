0363791ae788860fa03152a075a10c86208ac498
Melissa Falcão <falcaomelissa28@gmail.com>
2025-10-06 15:39:44 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.write("i'm back")
-st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F1266706123734727%2F&psig=AOvVaw0EpPXC7p7j-9vbD4C9kKZ9&ust=1759861742217000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMiE-YSakJADFQAAAAAdAAAAABAu", caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
+st.image("https://i.pinimg.com/736x/07/31/05/073105ca1489c3e6163b1194c9259ae5.jpg", caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
